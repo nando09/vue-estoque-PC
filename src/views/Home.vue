4cf42c685ee03aad27bd548619a16462f201d6f8
Fernando <nando.online@live.com>
2021-03-02 23:06:30 -0300

    Init login page and controller

---
+++ src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HelloWorld
+  },
+  created() {
+    let usuarioAux = sessionStorage.getItem('usuario')
+    if (usuarioAux) {
+      this.usuario = JSON.parse(usuarioAux)
+    } else {
+      this.$router.push('/login');
+    }
+
+    console.log(this.usuario);
+  },
+};
+</script>
---
+++ src/views/Login.vue
@@ -0,0 +1,95 @@
+<template>
+  <b-container class="bv-example-row body-login" fluid>
+    <b-row class="justify-content-md-center">
+      <b-col cols="12" class="position-absolute">
+        <h1 class="title-login"><span>S</span>tockys</h1>
+        <b-form-text id="password-help-block">
+          Aqui é seu sistema de estoque!
+        </b-form-text>
+      </b-col>
+      <b-col cols="12" md="auto" class="form-login">
+        <b-form @submit="onSubmit">
+          <label for="text-password">Usuário</label>
+          <b-input-group prepend="@" class="mb-2 mr-sm-2 mb-sm-0">
+            <b-form-input id="inline-form-input-username" v-model="usuario" placeholder="Usuário"></b-form-input>
+          </b-input-group>
+          <label for="text-password">Senha</label>
+          <b-form-input type="password" id="text-password" aria-describedby="password-help-block" v-model="senha"></b-form-input>
+          <b-row class="buttons">
+            <b-col>
+              <b-button type="submit" variant="success" block>Entrar</b-button>
+            </b-col>
+            <b-col>
+              <b-button  variant="info" block>Recuperar</b-button>
+            </b-col>
+          </b-row>
+        </b-form>
+      </b-col>
+    </b-row>
+</b-container></template>
+
+<script>
+// @ is an alias to /src
+import axios from "axios";
+
+export default {
+  name: "Login",
+  components: {},
+  data() {
+    return {
+      usuario: "fernando@doe.com",
+      senha: "12345"
+    };
+  },
+  methods: {
+    onSubmit(evt) {
+      evt.preventDefault();
+
+      let data = {
+        email: this.usuario,
+        password: this.senha
+      };
+
+      axios.post("http://backend.pitutinhos.com.br/api/login",data).then(function(response){
+          console.log(response);
+      }).catch(function (error) {
+          console.log(error);
+      });
+    },
+  }
+};
+</script>
+<style type="text/css">
+.buttons{
+  margin-top: 20px;
+}
+
+.form-login{
+  top: 25vh;
+  background: white;
+  box-shadow: 0 0 #f1f1f1;
+  border: 5px solid #c17d01;
+  border-radius: 5px;
+  padding: 30px;  
+}
+
+.body-login{
+  height: 100vh;
+  background: rgba(255, 193, 7, 0.3);
+}
+
+.title-login{
+  font-weight: bolder;
+  color: orange;
+  margin-top: 6%;
+}
+
+.title-login span{
+  color: #c17d01;
+}
+
+.input-group-text{
+  background: #ffecb4 !important;
+  color: #c17d01;
+}
+</style>
